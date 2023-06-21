b31240db9d63432e8a94cea7d887592c586fe0e0
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2023-06-21 22:01:26 +0200

    initial commit

---
+++ android/src/com/mrfield/flappygremlin/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mrfield.flappygremlin;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mrfield.flappygremlin.FlappyGremlin;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlappyGremlin(), config);
+	}
+}
