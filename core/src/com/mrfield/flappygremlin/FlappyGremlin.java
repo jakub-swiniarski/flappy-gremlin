cd4a7aa76fe398e503a82039d4bdfd120d7bea59
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2023-06-21 22:52:29 +0200

    4 trees

--- core/src/com/mrfield/flappygremlin/FlappyGremlin.java
+++ core/src/com/mrfield/flappygremlin/FlappyGremlin.java
@@ -1,19 +1,26 @@
 package com.mrfield.flappygremlin;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class FlappyGremlin extends ApplicationAdapter {
 	SpriteBatch batch;
 	Player player;
-	Tree tree;
+	Tree[] tree = new Tree[4];
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		player=new Player();
-		tree=new Tree();
+		for(int i=0; i<4; i++){
+			tree[i]=new Tree();
+			if(!(i%2==0)){
+				tree[i].img=new Texture(Gdx.files.internal("treeF.png"));
+			}
+		}
 	}
 
 	@Override
@@ -21,7 +28,9 @@ public class FlappyGremlin extends ApplicationAdapter {
 		ScreenUtils.clear(0, 1, 1, 1);
 		batch.begin();
 		batch.draw(player.img,player.rect.x,player.rect.y);
-		batch.draw(tree.img,tree.rect.x,tree.rect.y);
+		for(int i=0; i<4; i++){
+			batch.draw(tree[i].img,tree[i].rect.x,tree[i].rect.y);
+		}
 		batch.end();
 	}
 	
@@ -29,6 +38,8 @@ public class FlappyGremlin extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		player.img.dispose();
-		tree.img.dispose();
+		for(int i=0; i<4; i++){
+			tree[i].img.dispose();
+		}
 	}
 }
