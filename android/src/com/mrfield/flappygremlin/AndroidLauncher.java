97418bc8a158a49a28c75ca98c1e139cf3668250
PanPole <panpole91@gmail.com>
2023-06-23 23:06:36 +0200

    changed app icon

--- android/src/com/mrfield/flappygremlin/AndroidLauncher.java
+++ android/src/com/mrfield/flappygremlin/AndroidLauncher.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mrfield.flappygremlin.FlappyGremlin;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
