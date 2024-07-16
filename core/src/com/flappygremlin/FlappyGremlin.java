b636b9e763c27c1d2241295183d3a66586f23e15
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-07-16 16:54:32 +0200

    remove unused import

--- core/src/com/flappygremlin/FlappyGremlin.java
+++ core/src/com/flappygremlin/FlappyGremlin.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class FlappyGremlin extends ApplicationAdapter {
